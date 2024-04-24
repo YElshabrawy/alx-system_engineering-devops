b8eafcc954e8133015604f282bbbe6db6135841a
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-24 16:15:26 +0200

    1 name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,20 +3,22 @@
 import requests
 from sys import argv
 
-userid = int(argv[1])
-url_usr = f'https://jsonplaceholder.typicode.com/users/{userid}'
-url_todos = 'https://jsonplaceholder.typicode.com/todos'
-count = 0
-completed = 0
-completed_todos = []
 
-username = requests.get(url_usr).json()["name"]
-todos = requests.get(url_todos).json()
-for todo in todos:
-    if (todo["userId"] == userid):
-        count += 1
-        if (todo["completed"]):
-            completed += 1
-            completed_todos.append(todo["title"])
-print(f"Employee {username} is done with tasks({completed}/{count}):")
-[print(x) for x in completed_todos]
+if __name__ == "__main__":
+    userid = int(argv[1])
+    url_usr = f'https://jsonplaceholder.typicode.com/users/{userid}'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
+    count = 0
+    completed = 0
+    completed_todos = []
+
+    username = requests.get(url_usr).json()["name"]
+    todos = requests.get(url_todos).json()
+    for todo in todos:
+        if (todo["userId"] == userid):
+            count += 1
+            if (todo["completed"]):
+                completed += 1
+                completed_todos.append(todo["title"])
+    print(f"Employee {username} is done with tasks({completed}/{count}):")
+    [print(x) for x in completed_todos]
