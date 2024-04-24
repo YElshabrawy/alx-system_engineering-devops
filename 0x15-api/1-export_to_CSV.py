7ab1a8cca80ac1f3038e54879cbd6dcf265b0d7b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-24 16:32:03 +0200

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''gg'''
+from sys import argv
+import requests
+import csv
+
+
+if __name__ == "__main__":
+    userid = int(argv[1])
+    url_usr = f'https://jsonplaceholder.typicode.com/users/{userid}'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
+
+    username = requests.get(url_usr).json()["name"]
+    todos = requests.get(url_todos).json()
+    filename = f"{userid}.csv"
+    with open(filename, "w") as f:
+        for todo in todos:
+            if (todo["userId"] == userid):
+                line = f'"{userid}","{username}"'
+                line += f',"{todo["completed"]}","{todo["title"]}"\n'
+                f.write(line)
+    # Remove the last line
+    with open(filename, "rb+") as f:
+        f.seek(-1, 2)
+        f.truncate()
