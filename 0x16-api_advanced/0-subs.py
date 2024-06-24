15dcfa1777f37d62378fb405b2f255eccd66d126
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-06-24 23:50:06 +0300

    ..

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# This script prints the number of subscribers for a given subreddit.
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
