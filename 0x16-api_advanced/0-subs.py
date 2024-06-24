7bf8af795114c124546be7b2f7e60ee80d82f4f4
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-06-24 23:57:46 +0300

    ..

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-    return response.json().get('data').get('subscribers')
+    response = requests.get(url, headers=headers).json()
+    num = response.get('data', {}).get('subscribers', 0)
+    return num
