b62d2573cc270185efc2a9b744f6009d1c5077de
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-06-25 00:01:33 +0300

    ..

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Unfortunately I had to use a custom one'}
-    req = requests.get(url, headers=headers).json()
-    nums = req.get('data', {}).get('subscribers', 0)
-    return nums
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'youssef/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
